a688f4c3034238bfd506fb906870a61275e54e25
Davor Bagarić <davor.bagaric@rebellis.hr>
2014-02-18 19:13:51 +0100

    Some changes

--- lib/active_admin/globalize/index_table_for_extension.rb
+++ lib/active_admin/globalize/index_table_for_extension.rb
@@ -6,7 +6,7 @@ module ActiveAdmin
       def translation_status
         column I18n.t("active_admin.globalize.translations") do |obj|
           obj.translations.pluck(:locale).each do |t|
-            '<span class="status_tag">%s</span>' % t.to_s
+            "<span class=\"status_tag\">#{t.to_s}</span>"
           end.join(" ").html_safe
         end
       end
