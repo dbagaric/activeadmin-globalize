8b8aa08ab5d459a6ef7803808fb7cbba30cb4c03
Davor Bagarić <davor.bagaric@rebellis.hr>
2014-02-18 19:00:36 +0100

    Add short name

--- lib/active_admin/globalize/index_table_for_extension.rb
+++ lib/active_admin/globalize/index_table_for_extension.rb
@@ -4,7 +4,7 @@ module ActiveAdmin
   module Globalize
     module IndexTableFor
       def translation_status
-        column I18n.t("active_admin.globalize.translations") do |obj|
+        column I18n.t("active_admin.globalize.translations.short") do |obj|
           obj.translation_names.map do |t|
             '<span class="status_tag">%s</span>' % t
           end.join(" ").html_safe
